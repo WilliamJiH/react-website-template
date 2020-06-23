ee1f55f9648cdec75ef43e0ef986725bd69eb386
William Ji <williamji12345@gmail.com>
2020-06-23 16:49:59 -0400

    Changed Dropdown option to hover

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,19 +4,26 @@ import "./style.css";
 
 const Header = (props) => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-            <a className="navbar-brand" href="/">
-                <img src={require("../images/logo128.png")} alt={"Logo"}/>
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarToggler"
-                    aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"/>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li className={"nav-item"}><a className="nav-link" href="/">首页</a></li>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="d-flex flex-grow-1">
+                <span className="w-100 d-lg-none d-block"/>
+                <a className="navbar-brand" href="/">
+                    {/*<img src={require("../images/logo128.png")} alt={"Logo"}/>*/}
+                    海马国际教育
+                </a>
+                <div className="w-100 text-right">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbar">
+                        <span className="navbar-toggler-icon"/>
+                    </button>
+                </div>
+            </div>
+            <div className="collapse navbar-collapse flex-grow-1 text-right" id="navbar">
+                <ul className="navbar-nav ml-auto flex-nowrap">
+                    <li className="nav-item active">
+                        <a href="/" className="nav-link">首页 <span className={"sr-only"}>(current)</span></a>
+                    </li>
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button"
+                        <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             国际课程
                         </a>
@@ -25,10 +32,18 @@ const Header = (props) => {
                             <a className="dropdown-item" href="/">A-Level 课程</a>
                         </div>
                     </li>
-                    <li className={"nav-item"}><a className="nav-link" href="/">语言培训</a></li>
-                    <li className={"nav-item"}><a className="nav-link" href="/">竞赛项目</a></li>
-                    <li className={"nav-item"}><a className="nav-item nav-link" href="/">背景提升</a></li>
-                    <li className={"nav-item"}><a className="nav-item nav-link" href="/">关于我们</a></li>
+                    <li className="nav-item">
+                        <a href="/" className="nav-link">语言培训</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/" className="nav-link">竞赛项目</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/" className="nav-link">背景提升</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/" className="nav-link">关于我们</a>
+                    </li>
                 </ul>
             </div>
         </nav>
