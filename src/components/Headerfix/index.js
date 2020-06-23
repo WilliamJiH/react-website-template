a524bb8a2d42ae43585bda8470fc55a35c11d87b
William Ji <williamji12345@gmail.com>
2020-06-23 16:42:08 -0400

    Created a fixed header

---
+++ src/components/Headerfix/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+const Headerfix = () => {
+    return (
+        <div className="topnav">
+            <div className="topnav-right">
+                <a href="/">7/24 真人客服</a>
+                <a href="/">联系我们</a>
+                <a href="/">中文 </a>
+                <a href="/">English</a>
+            </div>
+        </div>
+    );
+};
+
+export default Headerfix;
