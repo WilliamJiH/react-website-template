cff2327f95f8363b193c092d47ca671d76fe7e7e
William Ji <williamji12345@gmail.com>
2020-06-23 18:50:54 -0400

    Added service introduction

---
+++ src/containers/Home/Services/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+const Services = () => {
+    return (
+        <div className={"Services"}>
+            <div className={"text-h1-inner"}>
+                <h1>为什么选择海马教育</h1>
+            </div>
+            <div className={"text-p4-inner"}>
+                <h2>四大专属服务</h2>
+            </div>
+            <div className={"text-p4-inner"}>
+                <h2>三大质量保障</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
