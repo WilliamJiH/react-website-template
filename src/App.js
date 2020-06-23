029ea7eb8eacfaaed024162d011aba8e09528213
William Ji <williamji12345@gmail.com>
2020-06-23 18:54:28 -0400

    Updated content inside our app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import Home from "./containers/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header";
+import Headerfix from "./components/Headerfix";
+
 
 function App() {
     return (
         <div className="App">
+            <Headerfix/>
             <Header/>
             <Home/>
         </div>
