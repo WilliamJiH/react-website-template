bc3574f018275cc39ab432b298ac2fb108e47b40
William Ji <williamji12345@gmail.com>
2020-06-25 01:32:57 -0400

    Updated contents

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import Home from "./containers/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header";
-import Headerfix from "./components/Headerfix";
+// import Headerfix from "./components/Headerfix";
+import Footer from "./components/Footer";
 
 
 function App() {
     return (
         <div className="App">
-            <Headerfix/>
+            {/*<Headerfix/>*/}
             <Header/>
             <Home/>
+            <Footer/>
         </div>
     );
 }
