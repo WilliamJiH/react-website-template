232d2ded7bb373f76f6dc577a0598a8b1a8b5595
William Ji <williamji12345@gmail.com>
2020-06-25 01:31:37 -0400

    Temporary disabled services

--- src/containers/Home/Services/index.js
+++ src/containers/Home/Services/index.js
@@ -10,6 +10,40 @@ const Services = () => {
             <div className={"text-p4-inner"}>
                 <h2>四大专属服务</h2>
             </div>
+            {/*<div className="row justify-content-center">*/}
+            {/*    <div className="col-md-3">*/}
+            {/*        <div className={"row"}>*/}
+            {/*            <div className={"col-image col-sm-4"}>*/}
+            {/*                <img src={require("../images/guihua.png")} alt={"plan"} width={"40"} height={"40"}/>*/}
+            {/*            </div>*/}
+            {/*            <div className={"col-text col-sm-8"}>学术规划</div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="col-md-3">*/}
+            {/*        <div className={"row"}>*/}
+            {/*            <div className={"col-image col-sm-4"}>*/}
+            {/*                <img src={require("../images/fudao.png")} alt={"counsel"} width={"40"} height={"40"}/>*/}
+            {/*            </div>*/}
+            {/*            <div className={"col-text col-sm-8"}>学术辅导</div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="col-md-3">*/}
+            {/*        <div className={"row"}>*/}
+            {/*            <div className={"col-image col-sm-4"}>*/}
+            {/*                <img src={require("../images/ducu.png")} alt={"supervise"} width={"40"} height={"40"}/>*/}
+            {/*            </div>*/}
+            {/*            <div className={"col-text col-sm-8"}>学术督促</div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="col-md-3">*/}
+            {/*        <div className={"row"}>*/}
+            {/*            <div className={"col-image col-sm-4"}>*/}
+            {/*                <img src={require("../images/fankui.png")} alt={"feedback"} width={"40"} height={"40"}/>*/}
+            {/*            </div>*/}
+            {/*            <div className={"col-text col-sm-8"}>学术反馈</div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
             <div className={"text-p4-inner"}>
                 <h2>三大质量保障</h2>
             </div>
