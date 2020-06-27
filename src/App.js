5aea2dcd99db3d1af16ba793b165ed678fb0b6b8
William Ji <williamji12345@gmail.com>
2020-06-27 02:04:06 -0400

    Used react-route-dom package

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
-import Home from "./containers/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from "./components/Header";
-// import Headerfix from "./components/Headerfix";
-import Footer from "./components/Footer";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./containers/Home";
+import About from "./containers/AboutUs";
+import Background from "./containers/Background";
+import Language from "./containers/Language";
+import FourOFour from "./containers/404";
 
 
 function App() {
     return (
-        <div className="App">
-            {/*<Headerfix/>*/}
-            <Header/>
-            <Home/>
-            <Footer/>
-        </div>
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path={"/"} exact={true} component={Home}/>
+                    <Route path={"/about"} exact={true} component={About}/>
+                    <Route path={"/background"} exact={true} component={Background}/>
+                    <Route path={"/language"} exact={true} component={Language}/>
+                    <Route path={"/404"} exact={true} component={FourOFour}/>
+                </Switch>
+            </div>
+        </Router>
+
     );
 }
 
