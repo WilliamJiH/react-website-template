64430578d3119f5c706d3cf9660c67a819f1e552
William Ji <williamji12345@gmail.com>
2020-06-27 02:02:51 -0400

    Added link for different navigator

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ const Header = (props) => {
             <div className="d-flex flex-grow-1">
                 <span className="w-100 d-lg-none d-block"/>
                 <a className="navbar-brand" href="/">
-                    {/*<img src={require("../images/logo128.png")} alt={"Logo"}/>*/}
+                    {/*<images src={require("../images/logo128.png")} alt={"Logo"}/>*/}
                     海马国际教育
                 </a>
                 <div className="w-100 text-right">
@@ -19,29 +19,29 @@ const Header = (props) => {
             <div className="collapse navbar-collapse flex-grow-1 text-right" id="navbar">
                 <ul className="navbar-nav ml-auto flex-nowrap">
                     <li className="nav-item active">
-                        <a href="/" className="nav-link">首页 <span className={"sr-only"}>(current)</span></a>
+                        <a href={"/"} className="nav-link">首页</a>
                     </li>
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown"
+                        <a className="nav-link dropdown-toggle" href="/ap" id="navbarDropdown"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             国际课程
                         </a>
                         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="/">AP 课程</a>
-                            <a className="dropdown-item" href="/">A-Level 课程</a>
+                            <a className="dropdown-item" href="/ap">AP 课程</a>
+                            <a className="dropdown-item" href="/alevel">A-Level 课程</a>
                         </div>
                     </li>
                     <li className="nav-item">
-                        <a href="/" className="nav-link">语言培训</a>
+                        <a href={"/language"} className="nav-link">语言培训</a>
                     </li>
                     <li className="nav-item">
-                        <a href="/" className="nav-link">竞赛项目</a>
+                        <a href="/contest" className="nav-link">竞赛项目</a>
                     </li>
                     <li className="nav-item">
-                        <a href="/" className="nav-link">背景提升</a>
+                        <a href="/background" className="nav-link">背景提升</a>
                     </li>
                     <li className="nav-item">
-                        <a href="/" className="nav-link">关于我们</a>
+                        <a href="/about" className="nav-link">关于我们</a>
                     </li>
                 </ul>
             </div>
