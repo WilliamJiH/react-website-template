76f67279a53974534228fa06a090ad574a4bab67
William Ji <williamji12345@gmail.com>
2020-06-30 22:20:07 -0400

    Fixed wrong character

--- src/containers/Home/CompanyIntro/index.js
+++ src/containers/Home/CompanyIntro/index.js
@@ -10,7 +10,7 @@ const CompanyIntro = (props) => {
                         <h2>Empower International Students to <span className={"excel"}>Excel</span>.</h2>
                     </div>
                     <div className="col-lg-8">
-                        <p className="lead">Easy Education
+                        <p className="lead">
                             海马国际教育是专注于国际课程培训的机构。主要业务有三大国际课程体系AP/ALevel/IB的培训、国际主流竞赛指导及留
                             学相关业务公司由名校海归联合教育行业资深人士创立。</p>
                     </div>
